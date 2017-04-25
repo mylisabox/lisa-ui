a79b074431ef1bbe012ebf12fe0edebb5eb1053d
Jimmy Aumard <Jimmy.Aumard@nestle.com>
2017-04-25 14:32:40 +0200

    always return index.html

--- server.js
+++ server.js
@@ -3,4 +3,7 @@
 const express = require('express');
 const server = express();
 server.use('/', express.static(__dirname + '/bundle'));
+server.get('*', function (req, res) {
+  res.sendfile('bundle/index.html');
+});
 server.listen(4200);
